a343107e0334557c15019ad551b6d52c2c2a5ec9
sayedsaid <sayedsaid707@gmail.com>
2018-10-15 09:32:41 +0200

    Get data from Fake Json Server using HTTPURLConnection

--- app/src/main/java/com/example/android/webserviceinarabic/MainActivity.java
+++ app/src/main/java/com/example/android/webserviceinarabic/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void loadData(View view) throws IOException {
+    public void loadData(View view) {
 
 
         new Thread(new Runnable() {
